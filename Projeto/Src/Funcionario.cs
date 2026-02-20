656773ad08cdbeeb74207e5bbe2b317b85660b16
José de Senna <josespnt1990@gmail.com>
2026-02-20 16:56:58 -0300

    feat: aplica validacao e regras de negocio na classe Funcionario
    - Aplica SolucaoProblema no programa principal

--- Projeto/Src/Funcionario.cs
+++ Projeto/Src/Funcionario.cs
@@ -10,7 +10,7 @@ class Funcionario
 
     public Funcionario(string nome, double salario, double imposto)
     {
-        if (nome != null && nome != "")
+        if (NomeValido(nome))
         {
             _nome = nome;
         }
@@ -19,8 +19,8 @@ class Funcionario
             _nome = "Funcionário";
         }
 
-        SalarioBruto = salario;
-        Imposto = imposto;
+        SalarioBruto = ValidarSalario(salario);
+        Imposto = ValidarImposto(imposto);
     }
 
     public string Nome
@@ -32,7 +32,7 @@ class Funcionario
 
         set
         {
-            if (value != null && value != "")
+            if (NomeValido(value))
             {
                 _nome = value;
             }
@@ -41,6 +41,42 @@ class Funcionario
 
     public Funcionario(string nome) : this(nome, 3000.0, 150.0) {}
 
+    private static bool NomeValido(string nome)
+    {
+        if (nome != null && nome != "")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private static double ValidarSalario(double salario)
+    {
+        if (salario >= 1000.0)
+        {
+            return salario;
+        }
+        else
+        {
+            return 1000.0;
+        }
+    }
+
+    private static double ValidarImposto(double imposto)
+    {
+        if (imposto >= 0.0 && imposto <= 1000.0)
+        {
+            return imposto;
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
+
     public double SalarioLiquido()
     {
         return SalarioBruto - Imposto;
@@ -48,7 +84,10 @@ class Funcionario
 
     public void AumentarSalario(double porcentagem)
     {
-        SalarioBruto+=SalarioBruto*(porcentagem/100.0);
+        if (porcentagem > 0.0)
+        {
+            SalarioBruto += SalarioBruto*(porcentagem/100.0);
+        }
     }
 
     public override string ToString()
